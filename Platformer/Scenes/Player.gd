3befb269d1f1bda9f7a6a26e901ec104d56d998b
JFerrio <90724066+JFerrio@users.noreply.github.com>
2021-09-28 16:46:36 -0400

    9/28

---
+++ Platformer/Scenes/Player.gd
@@ -0,0 +1,11 @@
+extends KinematicBody2D
+
+var velocity = Vector2 (0,0)
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = 100
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = 100
+	move_and_slide(velocity) 
+	print(velocity)
